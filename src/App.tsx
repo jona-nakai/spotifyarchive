0b00614f9de5aa8fd3504e69139e8d1b20bb5150
jona-nakai <jonanakai.dev@gmail.com>
2026-04-17 01:28:59 -0400

    small bug fixes to file upload process and features

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,45 @@ function App() {
   
   const [files, setFiles] = useState<File[]>([]);
   type FileStatus = "noFiles" | "uploading" | "validating" | "filesUploaded";
-  const [status, setStatus] = useState<FileStatus>("idle")
+  const [status, setStatus] = useState<FileStatus>("noFiles")
 
-  const handleDrop = (event: DragEvent) => {
+  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
     setStatus("uploading");
     const droppedFiles = event.dataTransfer.files;
+    const droppedFilesList = Array.from(droppedFiles)
     setStatus("validating")
     const isValid = validateFiles(droppedFiles);
-    if (isValid) => {
-      setFiles(droppedFiles)
+    if (isValid) {
+      setFiles(droppedFilesList)
       setStatus("filesUploaded")
     } else {
       setStatus("noFiles")
     }
   }
 
-  const handleDragOver = (event: DragEvent) => {
+  const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
   }
 
-  const handleInput = (event: ChangeEvent) => {
+  const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
     setStatus("uploading")
     const uploadedFiles = event.currentTarget.files;
     if (!uploadedFiles) return;
+    const uploadedFilesList = Array.from(uploadedFiles)
     setStatus("validating");
     const isValid = validateFiles(uploadedFiles)
-    if (isValid) => {
-      setFiles(uploadedFiles)
-      setStatus("filesUploaded")
+    if (isValid) {
+      setFiles(uploadedFilesList);
+      setStatus("filesUploaded");
     } else {
-      setStatus("noFiles")
+      setStatus("noFiles");
     }
   }
 
   const validateFiles = (droppedFiles: FileList) => {
     for (let i=0; i<droppedFiles.length; i++) {
-      const name = droppedFiles[i]
+      const name = droppedFiles[i].name
     }
   }
 
