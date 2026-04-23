ce33511ccfa26c96d9876b08129b77f8e883bd15
Jona Nakai <jonanakai.dev@gmail.com>
2026-04-23 00:07:19 -0400

    display stats for time periods on Stats page

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -7,8 +7,12 @@ function Stats() {
 
   return (
     <>
-      {totalStats === null ? <div>loading</div> : <div>{totalStats.count}</div>}
-      {totalStats === null ? <div>loading</div> : <div>{totalStats.total_ms_played}</div>}
+      {totalStats === null ? <div>loading</div> : <div>Total songs played: {totalStats.count}</div>}
+      {totalStats === null ? <div>loading</div> : <div>Milliseconds played: {totalStats.total_ms_played}</div>}
+      {totalStats === null ? <div>loading</div> : <div>Seconds played: {(totalStats.total_ms_played / 1000).toFixed(3)}</div>}
+      {totalStats === null ? <div>loading</div> : <div>Minutes played: {(totalStats.total_ms_played / 60000).toFixed(3)}</div>}
+      {totalStats === null ? <div>loading</div> : <div>Hours played: {(totalStats.total_ms_played / 3600000).toFixed(3)}</div>}
+      {totalStats === null ? <div>loading</div> : <div>Days played: {(totalStats.total_ms_played / 86400000).toFixed(3)}</div>}
     </> 
   )
 }
